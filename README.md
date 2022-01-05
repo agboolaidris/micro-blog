bc0186b24dffcc36db09fbb231b533f684ba6434
I_AM_IDRIS/ <66332778+agboolaidris@users.noreply.github.com>
2022-01-05 14:00:33 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,15 @@
+# Nextjs + MUI5 + Typescript + Emotions + Jest
+
+Boilerplate for Nextjs and material-ui 5 with some advance fixtures
+
+Steps to run this project:
+
+1. Run `yarn install` command
+2. Run `yarn dev` command
+
+stack:
+
+1. Nextjs
+2. typescript
+3. MUI
+4. Emotion
---
+++ src/pages/index.tsx
@@ -0,0 +1,22 @@
+import Head from 'next/head';
+
+const Home = () => (
+  <div>
+    <Head>
+      <title>Create Next App</title>
+      <link rel="icon" href="/favicon.ico" />
+    </Head>
+
+    <main>
+      <h1>
+        Welcome to <a href="https://nextjs.org">Next.js!</a>
+      </h1>
+
+      <p>
+        Get started by editing <code>pages/index.js</code>
+      </p>
+    </main>
+  </div>
+);
+
+export default Home;
