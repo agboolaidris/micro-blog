9b5d1db451e2da62b523af0ae9ba85ccd48aa62d
idris agboola <agboolaisholaidreez@gmail.com>
2022-01-06 17:06:31 +0100

    update

--- src/components/dashboardLayout/search.tsx
+++ src/components/dashboardLayout/search.tsx
@@ -1,51 +1,46 @@
 import React, { useState } from 'react';
-import { styled, alpha } from '@mui/material/styles';
+import { alpha, Theme } from '@mui/material/styles';
 import SearchIcon from '@mui/icons-material/Search';
 import InputBase from '@mui/material/InputBase';
-import _styled from '@emotion/styled';
+import styled from '@emotion/styled';
 import { Box } from '@mui/material';
 
-const SearchWrapper = _styled.div`
-    width: 500px;
+const SearchWrapper = styled.div<{ theme?: Theme }>`
+  width: 500px;
   position: relative;
+  border-radius: ${({ theme }) => theme.shape.borderRadius};
+  background-color: ${({ theme }) => alpha(theme.palette.common.white, 0.15)};
+  margin-right: ${({ theme }) => theme.spacing(2)};
+  margin-left: 0;
+  &:hover {
+    background-color: ${({ theme }) => alpha(theme.palette.common.white, 0.25)};
+  }
 `;
 
-// borderRadius: theme.shape.borderRadius,
-// backgroundColor: alpha(theme.palette.common.white, 0.15),
-// marginRight: theme.spacing(2),
-// marginLeft: 0,
-//   '&:hover': {
-//     backgroundColor: alpha(theme.palette.common.white, 0.25),
-//   },
+const SearchIconWrapper = styled.div<{ theme?: Theme }>`
+  padding: ${({ theme }) => theme.spacing(0, 2)};
+  height: 100%;
+  position: absolute;
+  pointer-events: none;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+`;
 
-// }));
+const StyledInputBase = styled(InputBase)<{ theme?: Theme }>`
+  color: inherit;
+  width: 100%;
+  .MuiInputBase-input {
+    padding: ${({ theme }) => theme.spacing(1, 1, 1, 0)};
+    padding-left: ${({ theme }) => `calc(1em + ${theme.spacing(4)})`};
+    transition: ${({ theme }) => theme.transitions.create('width')};
+  }
+`;
 
-const SearchIconWrapper = styled('div')(({ theme }) => ({
-  padding: theme.spacing(0, 2),
-  height: '100%',
-  position: 'absolute',
-  pointerEvents: 'none',
-  display: 'flex',
-  alignItems: 'center',
-  justifyContent: 'center',
-}));
-
-const StyledInputBase = styled(InputBase)(({ theme }) => ({
-  color: 'inherit',
-  width: '100%',
-  '& .MuiInputBase-input': {
-    padding: theme.spacing(1, 1, 1, 0),
-    paddingLeft: `calc(1em + ${theme.spacing(4)})`,
-    transition: theme.transitions.create('width'),
-  },
-}));
-
-const SearchOutput = _styled(Box)<{ open: boolean }>`
- position:absolute;
- top:100px;
- background:red;
- 
-
+const SearchOutput = styled(Box)<{ open: boolean }>`
+  position: absolute;
+  top: 100px;
+  background: red;
 `;
 
 function Search() {
